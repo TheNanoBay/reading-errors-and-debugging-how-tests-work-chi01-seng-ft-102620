22576f2396db3570b69ecbfad8ba550e41c660ad
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-09-27 21:17:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,11 @@
 first_number = 43 
 
 second_number = 16 
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number // second_number
